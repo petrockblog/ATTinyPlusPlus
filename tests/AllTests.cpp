8cb4eb1d23263a3c33b02c6b919c18a59e382d79
Florian <info@petrockblock.com>
2015-03-24 21:36:29 +0100

    CHG: Clean up

--- tests/AllTests.cpp
+++
@@ -1,9 +0,0 @@
-#include "CppUTest/CommandLineTestRunner.h"
-
-int main(int ac, char** av)
-{
-  const char * av_override[] = { "exe", "-v" }; //turn on verbose mode
-
-  //return CommandLineTestRunner::RunAllTests(ac, av);
-  return CommandLineTestRunner::RunAllTests(2, av_override);
-}
